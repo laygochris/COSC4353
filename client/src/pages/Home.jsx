1ac21ec5afc3b76dd2827f0378954193e5caf609
Tobamii <165857610+Tobamii@users.noreply.github.com>
2025-02-10 22:37:16 +0000

    Testing commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>home page</h1>
+      <h1>HELLO page</h1>
     </div>
   );
 };
