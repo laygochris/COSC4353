7852256c693eaf7cc9d37eed180d4abcaec9b1ae
christ0f3r <142352888+laygochris@users.noreply.github.com>
2025-02-14 16:46:49 -0600

    Edit navbar

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -6,7 +6,7 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import logo from "../images/ih_logo_navbar.png";
 import Notifications from "../pages/Notifications";
-
+import { CgProfile } from "react-icons/cg";
 
 const CustomNavbar = () => {
   return (
@@ -15,17 +15,15 @@ const CustomNavbar = () => {
         <Navbar.Brand as={Link} to="/home">
           <img src={logo} alt="Impact Houston" className="navbar-logo" />
         </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
+          <Nav className="mx-auto">
             <Nav.Link as={Link} to="/login">
               Login
             </Nav.Link>
             <Nav.Link as={Link} to="/eventManagement">
               Event Management
-            </Nav.Link>
-            <Nav.Link as={Link} to="/userProfile">
-              Profile
             </Nav.Link>
             <Nav.Link as={Link} to="/volunteerHistory">
               Volunteer History
@@ -35,6 +33,21 @@ const CustomNavbar = () => {
             </Nav.Link>
             <Nav.Link as={Link} to="/events">
               Events
+            </Nav.Link>
+          </Nav>
+
+          <Nav classname="ms-auto">
+            <Nav.Link as={Link} to="/userProfile">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                fill="currentColor"
+                class="bi bi-person"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+              </svg>
             </Nav.Link>
             <Notifications />
           </Nav>
