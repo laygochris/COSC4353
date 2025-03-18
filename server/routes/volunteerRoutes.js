913bd7e57b09826f41c1aef00709a67da2e6df71
christ0f3r <142352888+laygochris@users.noreply.github.com>
2025-03-18 12:59:46 -0500

    update volunteerRoutes.js

--- server/routes/volunteerRoutes.js
+++ server/routes/volunteerRoutes.js
@@ -1,10 +1,12 @@
-const express = require("express");
-const { getVolunteers, matchVolunteersToEvent, createVolunteer } = require("../controllers/volunteerController");
+const express = require('express');
+const { createVolunteerValidation, matchVolunteersToEventValidation } = require('../validators/volunteerValidator');
+const { getVolunteers, matchVolunteersToEvent, createVolunteer } = require('../controllers/volunteerController');
+const verifyToken = require('../middleware/verifyToken');
 
 const router = express.Router();
 
-router.get("/", getVolunteers); 
-router.get("/match/:volunteerId", matchVolunteersToEvent);
-router.post("/create", createVolunteer);
+router.get('/', verifyToken, getVolunteers);
+router.post('/create', verifyToken, createVolunteerValidation, createVolunteer);
+router.get('/match/:volunteerId', verifyToken, matchVolunteersToEventValidation, matchVolunteersToEvent);
 
 module.exports = router;
